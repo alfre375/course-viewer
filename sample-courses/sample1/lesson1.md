2653d94ff225ecca675a6aed12235ca933309fb1
Luna Solar de la Fuente <alfredesdef@gmail.com>
2025-11-08 22:02:28 -0500

    Created sample course; worked on course loading

---
+++ sample-courses/sample1/lesson1.md
@@ -0,0 +1,12 @@
+# Introduction
+This is a sample course. It is a course that is primarily intended to test the course-viewer application.
+
+# Get Started With Python
+The good-ol' hello world is as follows:
+```py
+print("Hello, world!")
+```
+There are a few key notes:
+- The `print` function is a builtin function that outputs a line of text to the console. By default, it also adds a newline character after the string
+- No semicolon — python does not use semicolons
+- The string `"Hello, world"`. String literals are declared in python with single or double quotes, and must use the same kind of quotation mark on both sides
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::fs;
+
 use gettextrs::*;
 use gtk::{gio::prelude::*, prelude::*, *};
 use rfd::FileDialog;
+use serde_json::Value;
 
 const APP_ID: &str = "dev.luna.courseviewer";
+const ETC_PATH: &str = "/etc/course-viewer";
 static mut COURSE_FILE: String = String::new();
 
 fn main() {
@@ -43,10 +47,34 @@ fn build_ui(app: &Application) {
         .margin_end(12)
         .build();
     
+    // Create a navigation menu
+    let navbar: Box = Box::new(Orientation::Vertical, 6);
+    navbar.append(&button);
+    
+    // Create a course selection menu
+    let csm: Box = Box::new(Orientation::Vertical, 6);
+    csm.append(&load_course);
+    
+    // Create a box with navbar and content
+    let main_box: Box = Box::new(Orientation::Horizontal, 6);
+    main_box.append(&navbar);
+    main_box.append(&csm);
+    
+    // Create a window and set the title
+    let window: ApplicationWindow = ApplicationWindow::builder()
+        .application(app)
+        .title(gettext("CourseViewer"))
+        .child(&main_box)
+        .maximized(true)
+        .build();
+    
+    // Present window
+    window.present();
+    
     load_course.connect_clicked(|_button: &Button| {
         let file_path: Option<std::path::PathBuf> = FileDialog::new()
-            .set_title(gettext("Choose a course file"))
-            .pick_file();
+            .set_title(gettext("Choose a course folder"))
+            .pick_folder();
         
         match file_path {
             Some(path) => {
@@ -62,30 +90,41 @@ fn build_ui(app: &Application) {
         }
         
         #[allow(static_mut_refs)]
+        let file_contents_res: Result<String, std::io::Error> = unsafe {
+            fs::read_to_string(COURSE_FILE.clone() + "/metadata.json")
+        };
+        
+        let mut parsed: Value = serde_json::from_str("{}").expect("Something went wrong");
+        
+        match file_contents_res {
+            Ok(data) => parsed = serde_json::from_str(&data).expect("Something went wrong while parsing data"),
+            Err(e) => println!("{:?}", e),
+        }
+        
+        let lesson_selector: Box = Box::new(Orientation::Vertical, 6);
+        let return_button: Button = Button::builder().label(gettext("Back to main menu")).build();
+        lesson_selector.append(&return_button);
+        
+        println!("{:?}", parsed["course_layout"].as_array().expect("Didn't work"));
+        for lesson in parsed["course_layout"].as_array().expect("Didn't work") {
+            println!("Lesson: {}",lesson["name"].as_str().expect("Something went wrong"));
+            let lesson_button: Button = Button::builder()
+                .label(lesson["name"].as_str().expect("Something went wrong"))
+                .build();
+            lesson_selector.append(&lesson_button);
+        }
+        
+        let window: ApplicationWindow = ApplicationWindow::builder()
+            .maximized(true)
+            .child(&lesson_selector)
+            .title(parsed["course_name"].as_str().expect("Didn't work"))
+            .build();
+        
+        window.present();
+        
+        println!("{:?}", parsed);
+        
+        #[allow(static_mut_refs)]
         unsafe { println!("{}", COURSE_FILE); }
     });
-    
-    // Create a navigation menu
-    let navbar: Box = Box::new(Orientation::Vertical, 12);
-    navbar.append(&button);
-    
-    // Create a course selection menu
-    let csm: Grid = Grid::new();
-    csm.attach(&load_course, 0, 0, 100, 100);
-    
-    // Create a box with navbar and content
-    let main_box: Box = Box::new(Orientation::Horizontal, 12);
-    main_box.append(&navbar);
-    main_box.append(&csm);
-    
-    // Create a window and set the title
-    let window: ApplicationWindow = ApplicationWindow::builder()
-        .application(app)
-        .title(gettext("CourseViewer"))
-        .child(&main_box)
-        .maximized(true)
-        .build();
-    
-    // Present window
-    window.present();
 }
